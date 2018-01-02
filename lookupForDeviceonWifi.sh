97007315a353f46f5630e8ee8e4945623a40f378
Cyril <cyril.parisot@gmail.com>
2018-01-02 17:45:12 +0100

    add description

--- lookupForDeviceonWifi.sh
+++ lookupForDeviceonWifi.sh
@@ -3,12 +3,12 @@ export LOGI_EMAIL=login
 export LOGI_PASS=pass
 export DEBUG=dsd
 export DeviceToLookFor=android-cyril
-
+# scan standard ip range
 if nmap -sn 192.168.1.0/24 | grep -q $DeviceToLookFor
 then
-   echo "device found";
+   echo "device found on local NetWork";
    node desactivateLogiCircle.js
 else
-   echo "device not found";
+   echo "device not found on local NetWork";
    node activateLogiCircle.js
 fi
