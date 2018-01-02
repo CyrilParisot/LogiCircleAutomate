a3940d701351daa8571899884d0e1dbce3e458d9
Cyril <cyril.parisot@gmail.com>
2018-01-02 17:49:44 +0100

    change js com

--- activateLogiCircle.js
+++ activateLogiCircle.js
@@ -4,17 +4,13 @@ const fs = require('fs')
 const fetch = require('node-fetch')
 const path = require('path')
 
-// convert cookie object to a name=value string
-// via https://github.com/mickdekkers/episodecalendar-exporter/blob/81f04e84c87bdeee1e67d0dc2b41d2b5960fdf2e/index.js#L96-L100
-
 
 async function startLogiCircle()  {
     const user = {
         email: process.env.LOGI_EMAIL,
         password: process.env.LOGI_PASS
     }
-    const browser = await puppeteer.launch() // change to "false" to inspect
-
+    const browser = await puppeteer.launch()
     const page = await browser.newPage()
 
     await page.goto('https://circle.logi.com')
@@ -29,8 +25,8 @@ async function startLogiCircle()  {
     await page.click('.krypto-login__button button')
 
     await page.waitFor(5000)
-    // waitForNavigation doesn't work in this app, neither does a waitFor(), so we do this:
-    await new Promise(resolve => setTimeout(() => resolve(), 15000))
+    // waitForNavigation doesn't work in this webapp, so workaround used :
+   await new Promise(resolve => setTimeout(() => resolve(), 15000))
 
     // debug('dismissing "subscribe" prompt')
     // await page.click('button.krypto-modalWindow__button')
--- desactivateLogiCircle.js
+++ desactivateLogiCircle.js
@@ -4,17 +4,13 @@ const fs = require('fs')
 const fetch = require('node-fetch')
 const path = require('path')
 
-// convert cookie object to a name=value string
-// via https://github.com/mickdekkers/episodecalendar-exporter/blob/81f04e84c87bdeee1e67d0dc2b41d2b5960fdf2e/index.js#L96-L100
-
 
 async function startLogiCircle()  {
     const user = {
         email: process.env.LOGI_EMAIL,
         password: process.env.LOGI_PASS
     }
-    const browser = await puppeteer.launch() // change to "false" to inspect
-
+    const browser = await puppeteer.launch()
     const page = await browser.newPage()
 
     await page.goto('https://circle.logi.com')
@@ -29,7 +25,7 @@ async function startLogiCircle()  {
     await page.click('.krypto-login__button button')
 
     await page.waitFor(5000)
-    // waitForNavigation doesn't work in this app, neither does a waitFor(), so we do this:
+    // waitForNavigation doesn't work in this webapp, so workaround used :
     await new Promise(resolve => setTimeout(() => resolve(), 15000))
 
     // debug('dismissing "subscribe" prompt')
